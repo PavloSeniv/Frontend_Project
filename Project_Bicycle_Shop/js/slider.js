de6ba4160c76c5233a799340f200c5f41ba9f570
OleksiiKuc <74044099+OleksiiKuc@users.noreply.github.com>
2021-05-25 20:58:00 +0300

    modified

--- Project_Bicycle_Shop/js/slider.js
+++ Project_Bicycle_Shop/js/slider.js
@@ -1,10 +1,22 @@
 $(function () {
 
+    /* Page 1 */
     $('.news__slider').slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
         prevArrow: '<button class="slick-btn slick-prev news__slick-prev"><img src="../img/left-arrow.png" alt="prev"></button>',
         nextArrow: '<button class="slick-btn slick-next news__slick-next"><img src="../img/right-arrow.png" alt="next"></button>'
+    });
+
+    /* Page 2 */
+    $('.category__slider').slick({
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        dots: true,
+        adaptiveHeight: true,
+        prevArrow: '<button class="slick-btn slick-prev"><img src="../img/images/left-arrow.png" alt="prev"></button>',
+        nextArrow: '<button class="slick-btn slick-next"><img src="../img/right-arrow.png" alt="next"></button>'
     });
 });
