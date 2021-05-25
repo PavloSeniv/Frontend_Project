e9db6e6ff2e1deca5ab5fcc630530e26a5a3c8e8
PavloSeniv <enazydragnil@gmail.com>
2021-05-25 16:01:53 +0300

    Page3_Seniv

---
+++ Project_Bicycle_Shop/Project_Bicycle_Shop_Page3/js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    $('.photo__slider').slick({
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        dots: true,
+        adaptiveHeight: true,
+        prevArrow: '<button class="slick-btn slick-prev"><img src="images/left-arrow.png" alt="prev"></button>',
+        nextArrow: '<button class="slick-btn slick-next"><img src="images/left-arrow.png" alt="next"></button>'
+    });
+
+});
---
+++ Project_Bicycle_Shop/Project_Bicycle_Shop_Page3/js/modal.js
@@ -0,0 +1,55 @@
+$(function() {    
+    const modalCall = $("[data-modal]");
+    const modalClose = $("[data-close]");
+
+    modalCall.on("click", function(event) {
+        event.preventDefault();
+
+        let $this = $(this); // Сохраняє значення дата атрибута, на який ми клікаємо. Тобто ми в цю кнопку сохраняємо дата атрибут або кнопки hire me або кнопки see my resume
+        let modalId = $this.data('modal'); // шукаємо ід класу modal
+
+        $(modalId).addClass('show');
+        $("body").addClass('no-scroll');
+
+        setTimeout(function() {
+            $(modalId).find(".modal__dialog").css({
+                transform: "rotateX(0)"
+            });
+        }, 200);
+
+        $('#worksSlider').slick('setPosition');
+    });
+
+    modalClose.on("click", function(event) {
+        event.preventDefault();
+
+        let $this = $(this); // Сохраняє значення дата атрибута, на який ми клікаємо. Тобто ми в цю кнопку сохраняємо дата атрибут або кнопки hire me або кнопки see my resume
+        let modalParent = $this.parents('.modal'); // знаходить батьківський класс modal. Тобто коли ми клієкамо по кнопці, воно шукає, в якому саме батьківському класі знахолиться кнопка.
+
+        modalParent.find(".modal__dialog").css({
+                transform: "rotateX(90deg)"
+            });
+
+        setTimeout(function() {
+            modalParent.removeClass('show');
+            $("body").removeClass('no-scroll');
+        }, 400);
+    });
+
+    $(".modal").on("click", function(event) {
+        let $this = $(this);
+
+        $(this).find(".modal__dialog").css({
+                transform: "rotateX(90deg)"
+            });
+
+       setTimeout(function() {
+            $this.removeClass('show');
+            $("body").removeClass('no-scroll');
+        }, 400);
+    }); // Потрібно, щоб модальне вікно закривалось, якщо клікнути поза ним
+
+    $(".modal__dialog").on("click", function(event) {
+        event.stopPropagation(); // Якщо ми клікаємо по елементу moda__dialog, то ми відміняємо клік по його батьківському класі
+    });
+});
