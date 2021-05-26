90be2d5c53de0664d774025ca2eb88ab3dd9ce0c
OleksiiKuc <74044099+OleksiiKuc@users.noreply.github.com>
2021-05-26 11:23:29 +0300

    add

--- Project_Bicycle_Shop/js/slider.js
+++ Project_Bicycle_Shop/js/slider.js
@@ -6,7 +6,23 @@ $(function () {
         slidesToShow: 3,
         slidesToScroll: 3,
         prevArrow: '<button class="slick-btn slick-prev news__slick-prev"><img src="img/left-arrow.png" alt="prev"></button>',
-        nextArrow: '<button class="slick-btn slick-next news__slick-next"><img src="img/right-arrow.png" alt="next"></button>'
+        nextArrow: '<button class="slick-btn slick-next news__slick-next"><img src="img/right-arrow.png" alt="next"></button>',
+        responsive: [
+            {
+              breakpoint: 1055,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+        ]
     });
 
     /* Page 2 */
