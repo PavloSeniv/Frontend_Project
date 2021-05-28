b11cd25fc4b323b9a3b240439582fa7ec6e568f8
PavloSeniv <enazydragnil@gmail.com>
2021-05-28 19:19:34 +0300

    Fix input Name for Alert window in page 1

--- Project_Bicycle_Shop/js/main.js
+++ Project_Bicycle_Shop/js/main.js
@@ -10,19 +10,8 @@ $(function () {
         nextArrow: '<button class="slick-btn slick-next"><img src="img/right-arrow.png" alt="next"></button>'
     });
 
-    (function () {
-        var addTime = document.getElementById('add');
-
-        addTime.addEventListener('click', getN);
-
-        function getN() {
-            var getNum = document.getElementById('get_num').value;
-            alert(
-                'Вітаємо ' +
-                getNum +
-                '.\nМожете ознайомитися з товарами в нашому магазині'
-            );
-        }
-    })();
-
 });
+
+function myFunction() {
+    alert("Вітаємо " + document.getElementById("myinput").value + ".\nМожете ознайомитися з товарами на сайті!!!");
+}
