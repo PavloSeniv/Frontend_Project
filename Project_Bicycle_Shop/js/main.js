2e3154f4d104e5a20ffaf13615ac663392237541
OleksiiKuc <74044099+OleksiiKuc@users.noreply.github.com>
2021-05-31 11:12:51 +0300

    modified json and add new photo

--- Project_Bicycle_Shop/js/main.js
+++ Project_Bicycle_Shop/js/main.js
@@ -1,3 +1,27 @@
 function myFunction() {
-    alert("Вітаємо " + document.getElementById("myinput").value + ".\nМожете ознайомитися з товарами на сайті!!!");
+
+    const toasts = document.querySelectorAll(".toast");
+    const toastTriggers = document.querySelectorAll(".toast__trigger");
+        
+    toastTriggers.forEach((trigger, index) => {
+      let toastTimeout;
+    
+      trigger.addEventListener("click", () => {
+        toasts[index].classList.add("toast--active");
+        toastTimeout = setTimeout(() => {
+          toasts[index].classList.remove("toast--active");
+        }, 10000);
+      });
+    
+      toasts[index].addEventListener("click", () => {
+        toasts[index].classList.remove("toast--active");
+        clearTimeout(toastTimeout);
+      });
+    });
+
+    var input = document.querySelector('#myinput').value;
+    document.getElementById("hello").innerHTML = input;
 }
+
+
+
